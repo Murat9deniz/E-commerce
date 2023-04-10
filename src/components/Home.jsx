ae72a9bd5f8f305d0160519ab295c186e8da74b3
zuhairseiba <121113038+zuhairseiba@users.noreply.github.com>
2023-04-10 15:34:05 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="hero">
       <div className="card text-bg-dark text-white border-0">
-        <img src="/assets/bg.jpeg" className="card-img" alt="Background" height="550px"/>
+        <img src="https://sem.arel.edu.tr/wp-content/uploads/2023/02/1_SwFB1o_k1LGprN-XRUZQ8w-1.jpeg" className="card-img" alt="Background" height="550px"/>
         <div className="card-img-overlay d-flex flex-column justify-content-center">
             <div className="container">
             </div>   
